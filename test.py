6d7aef55603d982b9dd373e2420808625dd6db91
hillinm <mark.hillin@gmail.com>
2024-09-10 17:14:35 -0500

    Organizing Files

--- test.py
+++
@@ -1,8 +0,0 @@
-animal_1 = "Animals"
-animal_2 = "Badger"
-bug_1 = "Honey Bee"
-bug_2 = "Honey Badger"
-print (animal_1.lower())
-print (animal_2.lower())
-print (bug_1.lower())
-print (bug_2.lower())
