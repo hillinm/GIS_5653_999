fbed30d89b57257a3a6dda31a76871cedfcfd35c
hillinm <mark.hillin@gmail.com>
2024-09-16 15:04:38 -0500

    Finished Chapter 6

---
+++ Jupyter_Notebooks/greetings.py
@@ -0,0 +1,11 @@
+def greetEnglish(name):
+    """Returns a good morning greeting in English."""
+    print("Good morning, " + str(name) + "!")
+    
+def greetGerman(name):
+    """Returns a good morning greeting in German."""
+    print(f"Guten Morgen, {name}!")
+
+def greetLatin(name):
+    """Returns a good morning greeting in Latin."""
+    print("Salve, {}!".format(name))
