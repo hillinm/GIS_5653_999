e8bb2409986c9709decf372337252f9bc93bf563
hillinm <mark.hillin@gmail.com>
2024-10-14 15:20:38 -0500

    Chapter 9 Exercises

---
+++ Chapter_9/9_1.py
@@ -0,0 +1,13 @@
+cardinal_numbers = ("first", "second", "third")
+print(cardinal_numbers[1])
+
+position1, position2, position3 = cardinal_numbers
+print(position1)
+print(position2)
+print(position3)
+
+my_name = tuple("Mark")
+
+print("x" in my_name)
+
+print(my_name[1:])
