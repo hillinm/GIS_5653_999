51666a2299beebe2420eb76101b2fc6d37f89e73
hillinm <mark.hillin@gmail.com>
2024-10-21 17:30:46 -0500

    Chapter 10

--- Chapter_10/10_2.py
+++ Chapter_10/10_2.py
@@ -5,9 +5,10 @@ opts, args = getopt.getopt(sys.argv, "?hH")
 
 class Dog:
     species = "Canis familiris"
-    def __init__(self, name, age):
+    def __init__(self, name, age, coat_color):
         self.name = name
         self.age = age
+        self.coat_color = coat_color
 
     def description(self):
         return f"{self.name} is {self.age} years old"
@@ -18,9 +19,10 @@ class Dog:
     def __str__(self):
         return f"{self.name} is {self.age} years old"
     
-buddy = Dog("Buddy", 9)
-miles = Dog("Miles", 4)
+buddy = Dog("Buddy", 9, "white")
+miles = Dog("Miles", 4, "black")
+philo = Dog("Philo", 5, "brown")
 print(miles.description())
 print(miles.speak("Woof Woof"))
-
 print(miles)
+print(f"{philo.name}'s coat is {philo.coat_color}")
---
+++ Chapter_10/10_2b.py
@@ -0,0 +1,8 @@
+import sys
+import getopt
+
+opts, args = getopt.getopt(sys.argv, "?hH")
+
+names = ["David","Dan","Joanna","Fletcher"]
+
+print(names)
